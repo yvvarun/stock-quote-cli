2cdad46c2463e1a2a682dfee83dea4d16e324ee9
yvvarun <yvvarun@gmail.com>
2018-08-22 16:24:15 +0530

    bug fixes

--- stock/stock_quote.py
+++ stock/stock_quote.py
@@ -1,4 +1,5 @@
 import sys
+import traceback
 import urllib2
 import json
 from collections import OrderedDict
@@ -12,6 +13,8 @@ def get_stock_info(symbol, data):
     stock_info["totalRevenue"] = data['totalRevenue']['fmt'] if 'fmt' in data['totalRevenue'] else "NA"
     stock_info["ebitda"] = data['ebitda']['fmt'] if 'fmt' in data['ebitda'] else "NA"
     stock_info["cashPerShare"] = data['totalCashPerShare']['fmt'] if 'fmt' in data['totalCashPerShare'] else "NA"
+    stock_info["currentRatio"] = data['currentRatio']['fmt'] if 'fmt' in data['currentRatio'] else "NA"
+    stock_info["quickRatio"] = data['quickRatio']['fmt'] if 'fmt' in data['quickRatio'] else "NA"
     stock_info["debtToEquity"] = data['debtToEquity']['fmt'] if 'fmt' in data['debtToEquity'] else "NA"
     stock_info["revenuePerShare"] = data['revenuePerShare']['fmt'] if 'fmt' in data['revenuePerShare'] else "NA"
     stock_info["grossProfits"] = data['grossProfits']['fmt'] if 'fmt' in data['grossProfits'] else "NA"
@@ -23,21 +26,28 @@ def get_stock_info(symbol, data):
     return stock_info
 
 def main():
-    if len (sys.argv) != 3 :
-        print "Usage: python stock_quote.py <SYMBOL> <EXCHANGE>"
+    if len (sys.argv) < 2 :
+        print "Usage: python stock_quote.py <SYMBOL> <EXCHANGE>\n"\
+              "NOTE: provide EXCHANGE only if the stock is not listed on NYSE"
         sys.exit(1)
 
-    symbol = sys.argv[1]
-    exchange = sys.argv[2]
-    url = "https://query1.finance.yahoo.com/v10/finance/quoteSummary/{0}.{1}?&modules=financialData".format(symbol, exchange)
-    response = urllib2.urlopen(url)
-    html = response.read()
-    d = json.loads(html)
+    try:
+        symbol = sys.argv[1]
+        if len(sys.argv) == 3:
+            exchange = "." + sys.argv[2]
+        else:
+            exchange = ""
+        url = "https://query1.finance.yahoo.com/v10/finance/quoteSummary/{0}{1}?&modules=financialData".format(symbol, exchange)
+        response = urllib2.urlopen(url)
+        html = response.read()
+        d = json.loads(html)
 
-    data = d['quoteSummary']['result'][0]['financialData']
-    stock_info = get_stock_info(symbol, data)
+        data = d['quoteSummary']['result'][0]['financialData']
+        stock_info = get_stock_info(symbol, data)
 
-    print(json.dumps(stock_info, indent=4))
+        print(json.dumps(stock_info, indent=4))
+    except:
+        print "Failed to fetch stock info. Please check the SYMBOL and EXCHANGE (if provided)."
 
 if __name__ == "__main__":
     main()
